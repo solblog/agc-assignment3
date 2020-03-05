2a9a141847046a258820cdf1b6dfa7751950f6cb
sol <solitblogdoc@gmail.com>
2020-03-05 23:20:33 +0100

    Commit and finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+ displayPassword: boolean = false;
+ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ 
+ listTimebuttonClicked = new Array<any>();
+ listNumberButtonClicked = [];
+ // listTimebuttonClicked = []
+ counter = 0;
+ 
+
+ togglePassword(){
+   this.displayPassword = !this.displayPassword;
+   // this.listTimebuttonClicked.push(new Date().getTime());
+   this.listTimebuttonClicked.push(new Date().getTime());
+   this.listNumberButtonClicked.push(this.counter);
+   this.counter++;
+ }
+
+ updatePoition3to7(){
+  this.listNumberButtonClicked[3] = 7;
+ }
+
+
+ getColor(){
+  if (this.counter > 7)
+    return 'green';
+ }
+
+
 }
